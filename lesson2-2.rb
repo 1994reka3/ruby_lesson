7002ff7eb2e65a4c0f90b4b880bbf4586190f716
1994reka3 <rik.s.1005@gmail.com>
2020-11-26 00:16:12 +0900

    [update]ruby8章2回目終了

--- lesson2-2.rb
+++ lesson2-2.rb
@@ -69,11 +69,92 @@
 #   puts "りんごの生産地は青森です"
 # end
 
-total_price = 120
-if total_price > 100
-  puts "みかんを購入。所持金に余りあり"
-elsif total_price == 100
-  puts "みかんを購入。所持金は0円"
-else
-  puts "みかんを購入することができません"
+# total_price = 120
+# if total_price > 100
+#   puts "みかんを購入。所持金に余りあり"
+# elsif total_price == 100
+#   puts "みかんを購入。所持金は0円"
+# else
+#   puts "みかんを購入することができません"
+# end
+
+# puts "キーボードからなにか入力してみましょう"
+# input_key = gets
+# puts "入力された内容は" + input_key
+
+# puts "キーボードで2つの数字を入力してください"
+# a = gets.to_i
+# b = gets.to_i
+# puts "2つの数字を足した合計は#{a+b}です"
+
+# dice = 0
+# while dice != 6 do
+#   dice = rand(1..6)
+#   puts dice
+# end
+
+# for i in 1..10 do
+#   puts i
+# end
+
+# {:apple => 120, :strawberry => 130, :orange => 150}.each do |fruit, price|
+#   puts "#{fruit}は#{price}円です。"
+# end
+
+# i = 0
+# while i <= 10 do
+#   if i > 5
+#     break
+#   end
+#   puts i
+#   i += 1
+# end
+
+# puts "計算を始めます"
+# puts "2つの値を入力してください"
+# a = gets.to_i
+# b = gets.to_i
+# puts "計算結果を出力します"
+# puts "a*b= #{a*b}"
+# puts "計算を終了します"
+
+puts "計算を始めます"
+puts "何回計算を繰り返しますか？"
+count = gets.to_i
+i = 1
+while i <= count do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a = #{a}"
+  puts "b = #{b}"
+  puts "計算結果を出力します"
+  puts "a + b = #{a+b}"
+  puts "a - b = #{a-b}"
+  puts "a * b = #{a*b}"
+  puts "a / b = #{a/b}"
+  i += 1
 end
+puts "計算を終了します"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
